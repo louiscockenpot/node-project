4b7ed97788241780e7c10487ab3799abd62b1c66
soly2812 <129759112+soly2812@users.noreply.github.com>
2023-12-31 21:45:10 +0100

    Add lesso page component

--- frontend/src/app/lesson-detail-page/lesson-detail-page.component.ts
+++ frontend/src/app/lesson-detail-page/lesson-detail-page.component.ts
@@ -6,17 +6,7 @@ import {ActivatedRoute, Router} from "@angular/router";
   templateUrl: './lesson-detail-page.component.html',
   styleUrls: ['./lesson-detail-page.component.css']
 })
-export class LessonDetailPageComponent {
-  readonly initialId: number;
-  id: number | undefined;
-  constructor(activatedRoute: ActivatedRoute, private router: Router) {
-    this.initialId = +activatedRoute.snapshot.params['id'];
-    console.log('initialId:', this.initialId);
-    activatedRoute.params.subscribe(currParams => {
-      this.id = +currParams['id'];
-      console.log('id:', this.id);
-    });
-  }
+
   onClickGoNextPage(){
     const nextId = (this.id)? this.id + 1 : 1;
     this.router.navigate(['/lesson', nextId]);
