9e73f6e994b79d788b3a5eef2bd68d514bea472c
louiscockenpot <louis.cockenpot@edu.devinci.fr>
2024-01-01 00:17:44 +0100

    Added/Removed features on navigation bar

--- frontend/src/app/menu-nav-bar/menu-nav-bar.component.ts
+++ frontend/src/app/menu-nav-bar/menu-nav-bar.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
 import { AuthService } from '../auth.service'; // Import your authentication service
 import { Router } from '@angular/router'; // Import Router for navigation
+import { NgbModal, ModalDismissReasons, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-menu-nav-bar',
@@ -8,7 +9,9 @@ import { Router } from '@angular/router'; // Import Router for navigation
   styleUrls: ['./menu-nav-bar.component.css'],
 })
 export class MenuNavBarComponent implements OnInit {
-  constructor(private authService: AuthService, private router: Router) {}
+  @ViewChild('content', { static: false }) content!: TemplateRef<any>;
+
+  constructor(private authService: AuthService, private router: Router, private modalService: NgbModal) {}
 
   ngOnInit(): void {}
 
@@ -24,5 +27,9 @@ export class MenuNavBarComponent implements OnInit {
   logout(): void {
     this.authService.logout(); // Implement this method in your AuthService
     this.router.navigate(['/login']); // Navigate to the login page after logout
+  }
+
+  open() {
+    this.modalService.open(this.content, { ariaLabelledBy: 'modal-basic-title' });
   }
 }
