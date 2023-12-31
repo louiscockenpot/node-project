673ed610240cbe12c7b3e55f189127081227d096
soly2812 <129759112+soly2812@users.noreply.github.com>
2023-12-31 22:01:51 +0100

    updating loginComponents

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -8,5 +8,21 @@ import { Router } from '@angular/router'; // Import Router for navigation
   styleUrls: ['./login.component.css'],
 })
 
+  export class LoginComponent {
+  credentials = { email: '', username: '' };
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login(): void {
+    this.authService.login(this.credentials).subscribe(
+      (response) => {
+        // Handle successful login, e.g., set tokens or user information
+        this.router.navigate(['/lesson-list']); // Navigate to a protected page after login
+      },
+      (error) => {
+        // Handle login error, e.g., show an error message
+        console.error('Login error:', error);
+      }
+    );
   }
 }
